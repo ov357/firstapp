90254291afe26b6730f0285e5f81fbc12f314bad
ov357 <al.milicevic@free.fr>
2017-05-08 07:54:56 +0200

    with jinja

--- flask1.py
+++ flask1.py
@@ -6,6 +6,8 @@ Created on Thu Apr 27 07:58:45 2017
 """
 
 from flask import Flask
+from flask import render_template
+
 #import redis
 import feedparser
 
@@ -27,15 +29,8 @@ def storedb():
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed=feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>Headlines</h1>
-            <b>{0}</b> </br>
-            <i>{1}</i> </br>
-            <p>{2}</p> </br>
-        </body>
-    </html>""".format(first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    #first_article = feed['entries'][0]
+    return render_template("home.html", articles = feed['entries'])
 
 
 def index():
@@ -45,5 +40,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
-
 
